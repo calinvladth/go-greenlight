d364715759cb127a540403d0348fe04ab5c21cbd
calinvladth <calinvladth@icloud.com>
2024-05-08 00:24:53 +0300

    final chapter 3

---
+++ cmd/api/movies.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"greenlight.calinvladth.com/internal/data"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create movie ...")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movies{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
